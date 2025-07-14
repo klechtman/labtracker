ceaa5a17f7fc45d5075862dc46edffe62dc58842
klechtman <klechtman@tissuedynamics.com>
2025-07-14 13:48:59 +0300

    finetuned animations

--- src/components/table/ColumnNumber.svelte
+++ src/components/table/ColumnNumber.svelte
@@ -1,8 +1,13 @@
 <script>
+  import { fade } from 'svelte/transition';
   export let number;
 </script>
 
-<div class="font-title text-center px-0.5">
+<div 
+  class="font-title text-center px-0.5"
+  in:fade={{ duration: 200, easing: (t) => t }}
+  out:fade={{ duration: 200, easing: (t) => t }}
+>
   {number}
   <div class="h-0.5 bg-sky-800 mb-0.5"></div>
 </div> 
--- src/components/table/RowNumber.svelte
+++ src/components/table/RowNumber.svelte
@@ -1,17 +1,22 @@
 <script>
   import { hoveredRow } from '../../stores/tableStore';
+  import { fade } from 'svelte/transition';
   export let number;
   export let className = '';
   $: isHovered = $hoveredRow === number;
 </script>
 
 <div
-  class={`flex font-montserrat items-center select-none h-full transition-colors duration-100 ${className}
+  class={`flex font-montserrat items-center select-none h-full transition-colors duration-500 ${className}
   ${isHovered ? 'font-title' : 'text-slate-400'}`}
   style="min-height: var(--cell-min-height, 32px); max-height: var(--cell-max-height, 48px);"
 >
   <span style="display: inline-block; width: 20px; text-align: center;">{number}</span>
   {#if isHovered}
-    <span class="ml-auto self-stretch w-0.5 bg-sky-800 my-0.5"></span>
+    <span 
+      class="ml-auto self-stretch w-0.5 bg-sky-800 my-0.5"
+      in:fade={{ duration: 200, easing: (t) => t }}
+      out:fade={{ duration: 200, easing: (t) => t }}
+    ></span>
   {/if}
 </div> 
